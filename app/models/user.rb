1c1d339a2f1abcaeb5a38acc96ffec82e1d4bf96
kota-makino <maxfafafa@gmail.com>
2019-02-23 00:57:11 +0900

    add sign in

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 16}, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,32}+\z/}
+
+  has_secure_password
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'pages#index'
+  get 'users/new'
+
+
+  resources :users
+end
