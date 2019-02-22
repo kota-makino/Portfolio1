6933cc42b786aa37b727f1210f08011a19062ab9
kota-makino <maxfafafa@gmail.com>
2019-02-23 02:12:23 +0900

    add log in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user =  User.find_by(email:email_params[:email])
+    if user && user.authenticate(password_params[:password])
+      log_in user
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+end
+
+private
+def email_params
+  params.require(:session).permit(:email)
+end
+
+def password_params
+  params.require(:session).permit(:password)
+end
+
+def log_in(user)
+  session[:user_id] = user.id
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'pages#index'
   get 'users/new'
 
-
   resources :users
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
 end
